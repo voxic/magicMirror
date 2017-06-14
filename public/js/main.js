4f95de59f179063eeecacb45b57339af0afd4e41
Emil Nilsson <emil.m.nilsson@atea.se>
2017-06-14 07:05:32 +0200

    beta ready

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,12 @@
-/*function startTime() {
-    var today = new Date();
-    console.log(today.getDay());
-    var h = today.getHours();
-    var m = today.getMinutes();
-    m = checkTime(m);
-    document.getElementById('time').innerHTML =
-    h + ":" + m;
-    var t = setTimeout(startTime, 10000);
-}
-function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-}*/
-
-
-$(function () {
-    //startTime();
+function startTime() {
     moment().locale('sv');
-    console.log(moment().locale('sv').format('LT'));
-    console.log(moment().locale('sv').format('dddd'));
     $("#time").text(moment().locale('sv').format('LT'));
     $("#day").text(moment().locale('sv').format('dddd'));
     $("#date").text(moment().locale('sv').format('LL'));
+    var t = setTimeout(startTime, 10000);
+}
+
+$(function () {
+    startTime();
+
 });
